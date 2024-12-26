3f7bc98856a98d82097512c9ff274c0f4590540e
mrvivekdev <tyou5638@gmail.com>
2024-12-26 15:28:04 +0530

    Adding About Page

--- src/Component/AboutMe.tsx
+++ src/Component/AboutMe.tsx
@@ -1,13 +1,44 @@
 import React from "react"
 import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+import Image from '../assets/IMG_20240819_204056_373.webp'
 
 const About: React.FC = () => {
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    useGSAP(()=>{
+        gsap.from("#ScrollTraggerImage",{
+            y: 1000,
+            duration: 2,
+            delay: .5,
+            // opacity: 0,
+            scrollTrigger:{
+                trigger:"#ScrollTragger",
+                scroller: "body",
+                scrub:2,
+                // markers: true,
+                start: "top -50%",
+                end: "top 20%",
+            }
+        })
+    })
+
     return(
         <>
-            <div className="h-screen w-screen bg-black flex justify-center mt-10 lg:mt-40 lg:flex-row">
-                <h1 className="select-none text-orange-200 text-2xl font-extrabold lg:font-bold lg:text-5xl">About Me!</h1>
-                <div className="h-32 w-32 border-solid">
-                    <img src="" alt="" />
+            <div id="ScrollTragger" className="h-screen w-screen bg-black flex justify-evenly items-center flex-col pt-0 overflow-x-hidden">
+                <h1 id="ScrollTraggerImage" className="select-none text-orange-200 text-2xl font-extrabold mt-10 lg:font-bold lg:text-5xl">About Me!</h1> 
+                <div className="h-full w-full bg-black flex flex-col justify-evenly lg:flex-row pt-0 mt-20 ml-24 mr-0 lg:mt-48">
+                    <div id="ScrollTraggerImage" className="h-72 w-72 border-8 border-solid border-orange-200 rounded-3xl lg:h-3/5 lg:w-1/4">
+                        <img src={Image} alt="MyImage" className="h-full w-full p-0 lg:h-full lg:w-full rounded-2xl"/>
+                    </div>
+                    <div id="ScrollTraggerImage" className="h-72 w-72 lg:h-2/4 lg:w-1/4 mt-10 lg:mt-8 ml-4">
+                        <span className="select-none text-l lg:text-4xl font-serif font-extrabold text-orange-200">
+                            Hi, I'm Vivek, a passionate and results-driven full-stack developer with a knack for building robust, user-centric web applications. With expertise in front-end and back-end technologies, I craft seamless digital experiences tailored to meet diverse needs.
+                        </span>
+                    </div> 
                 </div>
             </div>
         </>
@@ -15,3 +46,4 @@ const About: React.FC = () => {
 }
 
 export default About;
+
--- src/Component/LandingPage.tsx
+++ src/Component/LandingPage.tsx
@@ -16,6 +16,7 @@ const Landing: React.FC = () => {
             opacity: 0,
             duration: 1,
             delay: .6,
+            stagger:0.5
         })
     })
 
@@ -25,14 +26,24 @@ const Landing: React.FC = () => {
             <h1 className='select-none text-2xl font-bold text-orange-200 lg:text-2xl cursor-pointer' id='H1Gsap'>VIVEK.DEV</h1>
         </div>
         <div className='flex items-center w-full h-screen'>
-            <div className='w-1/3 h-3/5 ml-[130px] lg:ml-20 flex justify-center items-center'>
+            <div className='w-1/3 h-3/5 ml-[20px] lg:ml-10 flex flex-col justify-center '>
                 <span className='select-none text-5xl lg:text-8xl font-serif font-extrabold text-orange-200' id='MainText'>
-                    "Hey, Welcome! Let’s Build the Future of Web Development Together."                   
+                    "Hey,                   
                 </span> 
+                <span className='select-none text-5xl lg:text-8xl font-serif font-extrabold text-orange-200' id='MainText'>
+                    Welcome!                   
+                </span> 
+                <span className='select-none text-5xl lg:text-8xl font-serif font-extrabold text-orange-200' id='MainText'>
+                    Let’s Build               
+                </span>
+                <span className='select-none text-5xl lg:text-8xl font-serif font-extrabold text-orange-200' id='MainText'>
+                    Future of Web Development Together."                
+                </span>
             </div>
         </div>
     </>
   )
 }
+// Welcome! Let’s Build the Future of Web Development Together.
 
 export default Landing;
